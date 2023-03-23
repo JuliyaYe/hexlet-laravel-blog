d133a9b03c04624073c4d02f94b851ddaf9a69b8
JulyYe <esprostoes@gmail.com>
2023-03-23 19:17:11 +0500

    Blade train

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return 'hello, world!';
 });
+
+Route::get('/about', function () { return  view('about'); });
